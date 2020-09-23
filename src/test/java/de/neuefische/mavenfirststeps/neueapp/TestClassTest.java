244f5daeaa668973d93dd751e6a28b6dd01e4896
Kai Heinrich <kai.heinrich91@web.de>
2020-09-23 19:01:38 +0200

    Added boolean test

--- src/test/java/de/neuefische/mavenfirststeps/neueapp/TestClassTest.java
+++ src/test/java/de/neuefische/mavenfirststeps/neueapp/TestClassTest.java
@@ -7,12 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class TestClassTest {
     @Test
     public void test() {
-        int sum = TestClass.add(2,3);
+        int sum = Operations.add(2,3);
         assertEquals(5, sum);
     }
     @Test
     public void anotherTest() {
-        int doubleSum = 2* TestClass.add(4,7);
+        int doubleSum = 2 * Operations.add(4,7);
         assertEquals(22, doubleSum);
+    }
+    @Test
+    public void testIfGreater() {
+        boolean result = Operations.isGreater(8,7);
+        assertTrue(result);
     }
 }
